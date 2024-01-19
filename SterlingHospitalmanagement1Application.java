60a342759b28cacccd88c032018bae9da8e90ae5
Nikita Mali <126570356+nikitasmali@users.noreply.github.com>
2024-01-19 23:04:48 +0530

    Add files via upload

---
+++ SterlingHospitalmanagement1Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SterlingHospitalmanagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SterlingHospitalmanagement1Application.class, args);
+		System.out.println("Hospital Managemnt System Is Running...!");
+	}
+
+}
